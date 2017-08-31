a69d02cb3c3f6f73c87fc393c83d23b4d68bc14f
Tich <lewis.smith.tian@gmail.com>
2017-08-31 19:07:28 +0800

    create travis script

---
+++ test_travis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-08-31 18:59:29
+# @Author  : Lewis Tian (chasetichlewis@gmail.com)
+# @Link    : https://github.com/LewisTian
+# @Version : Python3.5
+
+import bs4
+import requests
+import openpyxl
+
+def main():
+    assert bs4.__version__ == "4.6.0"
+    assert requests.__version__ == "2.18.1"
+    assert openpyxl.__version__ == "2.18.1"
+
+
+if __name__ == '__main__':
+    main()
